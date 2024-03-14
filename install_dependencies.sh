10206c0d8f397a9317820fe10aae45029153ee7b
Brett Piggott <123497965+Bapiggott@users.noreply.github.com>
2024-03-14 10:30:12 -0400

    Create install_dependencies.sh

---
+++ install_dependencies.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+pip install jsonlib
+pip install python-csv
+pip install pandas
+pip install transformers==4.38.2
+pip install -U bitsandbytes
+pip install -U git+https://github.com/huggingface/peft.git
+pip install -U git+https://github.com/huggingface/accelerate.git
+pip install -U einops
+pip install -U safetensors
+pip install -U xformers
+pip install -U datasets
+pip install accelerate
+pip install appdirs
+pip install bitsandbytes
+pip install datasets
+pip install fire
+pip install torch
+pip install sentencepiece
+pip install tensorboardX
+pip install gradio
